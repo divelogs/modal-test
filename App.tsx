17e79c35a683ecaa77ae529f4677f0f0e2c38444
Rainer Mohr <mail@klemmkeil.de>
2024-03-17 22:06:49 +0100

    initial

--- App.tsx
+++ App.tsx
@@ -1,118 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const BottomNavigation = ()=> {
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const Tab = createBottomTabNavigator();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+  return (<View style={{flex: 1}}>
+  <NavigationContainer>
+    <Tab.Navigator screenOptions={{
+      tabBarStyle: { backgroundColor: '#3fb9f2'},
+      headerStyle: { backgroundColor: '#3fb9f2'},
+      headerTitle: () => <Text>Header Text</Text>,
+      headerTitleAlign: 'center'      
+    }}>
+      <Tab.Screen name="Something" options={{ 
+        title: "something",
+        headerShown: false, 
+        tabBarActiveTintColor: '#FFFFFF', 
+        tabBarInactiveTintColor: '#FFFFFF',
+        tabBarLabelStyle: {fontSize: 14}
+      }}>
+        {(props) => { return <DivesNavigation {...props}/>}}
+      </Tab.Screen>       
+    </Tab.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const DivesNavigation = ({navigation}:any) => {
+  const Stack = createNativeStackNavigator();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const focusDiveProfile = () => {
+    navigation.navigate("MyModalWindow");
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#3fb9f2'
+            },
+            headerTintColor: '#fff',
+            headerTitle: () => <Text>My header</Text>,
+            headerTitleAlign: 'center'           
+          }}          
+        >
+        
+        <Stack.Screen name="Demo">
+          {(props) => {return <Text>
+            <TouchableOpacity onPress={() => focusDiveProfile()}>
+              <Text style={{padding: 30}}>This is to demonstrate a problem with transparentModal on iOS devices 
+                with a notch or dynamic island. 
+                Click to open modal in LANDSCAPE mode and then close it.
+              </Text>
+            </TouchableOpacity>
+          </Text>}}
+        </Stack.Screen>
+        
+        <Stack.Screen
+          name="MyModalWindow"
+          component={Modalcontent}
+          options={{ presentation: 'transparentModal', headerShown: false }}
+        />   
+
+      </Stack.Navigator>   
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+const Modalcontent = ({navigation}:any) => {
+
+  const closeModal = () => {
+    navigation.pop()
+  }
+
+  return (
+    <View style={{flex: 1, padding: 30, backgroundColor: '#FFFFFF'}}>
+        <Text>This modal needs to be full screen, as it will contain a lot of (interactive) content and I do not want 
+          the title or bottom bar to show. Click on close in the next line, and the header and footer will be higher than before, 
+          taking up much more space until you force close the app:
+        </Text>
+        <TouchableOpacity onPress={closeModal}>
+            <Text style={{fontWeight: 'bold'}}>Close Modal</Text>
+        </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BottomNavigation
